037e5d4599795dc74f7a201f0b213ad5e225334c
Virat Srivastava <vsrivastava.dev@gmail.com>
2025-05-11 18:22:28 +0530

    bug fix :added / in delete controller

--- backend/src/routes/problem.routes.js
+++ backend/src/routes/problem.routes.js
@@ -12,7 +12,7 @@ problemRoutes.get('/get-problem/:id', authMiddleware, getProblemById)
 
 problemRoutes.put('update-problem/:id', authMiddleware, checkAdmin, updateProblem)
 
-problemRoutes.delete('delete-problem/:id',authMiddleware, checkAdmin, deleteProblem)
+problemRoutes.delete('/delete-problem/:id',authMiddleware, checkAdmin, deleteProblem)
 
 problemRoutes.get('/get-solved-problems', authMiddleware, getAllProblemsSolvedByUser)
 
